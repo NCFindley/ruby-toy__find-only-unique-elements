de343c2ce827afa18de26246733d386eacbe8ee9
Nels Findley <ncfindley2@gmail.com>
2016-06-15 00:36:19 +0000

    Nels Findley

--- lib/unique_elements.rb
+++ lib/unique_elements.rb
@@ -2,5 +2,17 @@
 # combination of the above), and returns an array of the elements that appear once and only
 # once.
 def find_unique_elements(arr)
+ 
+ newarr = []
+ 
+  arr.each do | x |
+  
+    if arr.count(x) == 1
+      newarr << x
+      
+    end
+  end
+  
+  return newarr
   
 end
